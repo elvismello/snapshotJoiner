cb35bf6d0efd435a46558e3527b40a6ab6501dec
elvismello <elvis.mello@outlook.com>
2022-02-28 13:53:59 -0300

    Added support to metallicity; Ported to h5py

--- snapshotJoiner.py
+++ snapshotJoiner.py
@@ -16,11 +16,12 @@ passing the necessary data to the function write_snapshot.
 
 
 import numpy as np
-import pynbody as pnb
+import h5py
 import snapwrite
+import argparse
 
 
-def rotation (vector, alpha=0, beta=0, gamma=0, returnMatrix=False):
+def rotation (vector, alpha=0, beta=0, gamma=0, returnMatrix=False, dtype='float64'):
     """
     alpha: angle of rotation around the x axis
     beta: angle of rotation around the y axis
@@ -32,22 +33,22 @@ def rotation (vector, alpha=0, beta=0, gamma=0, returnMatrix=False):
     #rotation matrix in x
     rAlpha = np.array([[1,             0,              0],
                        [0, np.cos(alpha), -np.sin(alpha)],
-                       [0, np.sin(alpha), np.cos(alpha)]])
+                       [0, np.sin(alpha), np.cos(alpha)]], dtype=dtype)
     
     #rotation matrix in y
     rBeta = np.array([[np.cos(beta),  0, -np.sin(beta)],
                       [0,             1,             0],
-                      [-np.sin(beta), 0, np.cos(beta)]])
+                      [-np.sin(beta), 0, np.cos(beta)]], dtype=dtype)
 
     #rotation matrix in z
     rGamma = np.array([[np.cos(gamma), -np.sin(gamma), 0],
                        [np.sin(gamma),  np.cos(gamma), 0],
-                       [0,                          0, 1]])
+                       [0,                          0, 1]], dtype=dtype)
 
-    rGeneral = np.matmul(np.matmul(rAlpha, rBeta), rGamma)
+    rGeneral = np.matmul(np.matmul(rAlpha, rBeta), rGamma, dtype=dtype)
 
     if not returnMatrix:
-        return np.matmul(rGeneral, np.array(vector))
+        return np.matmul(rGeneral, np.array(vector), dtype=dtype)
     else:
         return rGeneral
 
@@ -76,15 +77,14 @@ def join (snapshotZero, snapshotOne, output='init.dat',
     rotationAngles = np.radians([float(i) for i in rotationAngles])
 
     #standard families in gadget 2
-    particleFamilies = ['gas', 'dm', 'disk', 'bulge', 'stars', 'bndry']
+    particleTypes = ['PartType0', 'PartType1', 'PartType2', 'PartType3', 'PartType4', 'PartType5']
 
 
     #Loading snapshots
-    snapshotZero = pnb.load(snapshotZero)
-    snapshotOne = pnb.load(snapshotOne)
+    snapshotZero = h5py.File(snapshotZero)
+    snapshotOne = h5py.File(snapshotOne)
 
 
-    #TODO solve the problem of joining two snapshots with non-standart families
     #Getting information and joining each type of particle
     nPart = []
     positions = []
@@ -93,66 +93,73 @@ def join (snapshotZero, snapshotOne, output='init.dat',
     energy = []
     rho = []
     smoothing = []
+    metallicity = []
 
 
-    for i in particleFamilies:
+    for i in particleTypes:
 
-        if i != 'dm' or includeHaloZero: # skips writing the halo from the first snapshot if includeHaloZero is False
-            existsInZero = i in [*snapshotZero.families()]
+        if i != 'PartType1' or includeHaloZero: # skips writing the halo from the first snapshot if includeHaloZero is False
+            existsInZero = i in [*snapshotZero.keys()]
         else:
             existsInZero = False
                 
-        existsInOne = i in [*snapshotOne.families()]
+        existsInOne = i in [*snapshotOne.keys()]
 
         if existsInZero and existsInOne:
-            for j in snapshotZero.families():
-                if j == i: #loop needed in order to iterate over the families
-                    break
+            # Possibly not needed
+            #for j in snapshotZero.keys():
+            #    if j == i: #loop needed in order to iterate over the families
+            #        break
 
-            nPart.append(len(snapshotZero[j]['iord']) + len(snapshotOne[j]['iord']))
+            nPart.append(len(snapshotZero[i]['ParticleIDs']) + len(snapshotOne[i]['ParticleIDs']))
+            # Position and velocities
             if np.any(rotationAngles): #applies rotation if any given angle is different of 0
-                positions.append(np.concatenate((np.array(snapshotZero[j]['pos']),
-                                                 np.array([rotation(h, *rotationAngles) for h in snapshotOne[j]['pos']]) + relativePos)))
-                velocities.append(np.concatenate((np.array(snapshotZero[j]['vel']),
-                                                 np.array([rotation(h, *rotationAngles) for h in snapshotOne[j]['vel']]) + relativeVel)))
+                positions.append(np.concatenate((np.array(snapshotZero[i]['Coordinates']),
+                                                 np.array([rotation(h, *rotationAngles) for h in snapshotOne[i]['Coordinates']]) + relativePos)))
+                velocities.append(np.concatenate((np.array(snapshotZero[i]['Velocities']),
+                                                 np.array([rotation(h, *rotationAngles) for h in snapshotOne[i]['Velocities']]) + relativeVel)))
             else:
-                positions.append(np.concatenate((np.array(snapshotZero[j]['pos']), np.array(snapshotOne[j]['pos']) + relativePos)))
-                velocities.append(np.concatenate((np.array(snapshotZero[j]['vel']), np.array(snapshotOne[j]['vel']) + relativeVel)))               
-            masses.append(np.concatenate((np.array(snapshotZero[j]['mass']), np.array(snapshotOne[j]['mass']))))
-            if i == 'gas':
-                energy.append(np.concatenate((np.array(snapshotZero[j]['u']), np.array(snapshotOne[j]['u']))))
-                rho.append(np.concatenate((np.array(snapshotZero[j]['rho']), np.array(snapshotOne[j]['rho']))))
-                smoothing.append(np.concatenate((np.array(snapshotZero[j]['smooth']), np.array(snapshotOne[j]['smooth']))))
+                positions.append(np.concatenate((np.array(snapshotZero[i]['Coordinates']), np.array(snapshotOne[i]['Coordinates']) + relativePos)))
+                velocities.append(np.concatenate((np.array(snapshotZero[i]['Velocities']), np.array(snapshotOne[i]['Velocities']) + relativeVel)))               
+            # Masses
+            masses.append(np.concatenate((np.array(snapshotZero[i]['Masses']), np.array(snapshotOne[i]['Masses']))))
+            # Gas Properties
+            if i == 'PartType0':
+                energy.append(np.concatenate((np.array(snapshotZero[i]['InternalEnergy']), np.array(snapshotOne[i]['InternalEnergy']))))
+                rho.append(np.concatenate((np.array(snapshotZero[i]['Density']), np.array(snapshotOne[i]['Density']))))
+                smoothing.append(np.concatenate((np.array(snapshotZero[i]['SmoothingLength']), np.array(snapshotOne[i]['SmoothingLength']))))
+            # Metallicity
+            if i != 'PartType1':
+                metallicity.append(np.concatenate(np.array(snapshotZero[i]['Metallicity']), np.array(snapshotOne[i]['Metallicity'])))
+
         
         elif existsInZero:
-            for j in snapshotZero.families():
-                if j == i:
-                    break          
-            nPart.append(len(snapshotZero[j]['iord']))
-            positions.append(np.array(snapshotZero[j]['pos']))
-            velocities.append(np.array(snapshotZero[j]['vel']))
-            masses.append(np.array(snapshotZero[j]['mass']))
-            if i == 'gas':
-                energy.append(np.array(snapshotZero[j]['u']))
-                rho.append(np.array(snapshotZero[j]['rho']))
-                smoothing.append(np.array(snapshotZero[j]['smooth']))
+            nPart.append(len(snapshotZero[i]['ParticleIDs']))
+            positions.append(np.array(snapshotZero[i]['Coordinates']))
+            velocities.append(np.array(snapshotZero[i]['Velocities']))
+            masses.append(np.array(snapshotZero[i]['Masses']))
+            if i == 'PartType0':
+                energy.append(np.array(snapshotZero[i]['InternalEnergy']))
+                rho.append(np.array(snapshotZero[i]['Density']))
+                smoothing.append(np.array(snapshotZero[i]['SmoothingLength']))
+            if i != 'PartType1':
+                metallicity.append(np.concatenate(np.array(snapshotZero[i]['Metallicity'])))
         
         elif existsInOne:
-            for j in snapshotOne.families():
-                if j == i:
-                    break
-            nPart.append(len(snapshotOne[j]['iord']))
+            nPart.append(len(snapshotOne[i]['ParticleIDs']))
             if np.any(rotationAngles):
-                positions.append(np.array([rotation(h, *rotationAngles) for h in snapshotOne[j]['pos']]) + relativePos)
-                velocities.append(np.array([rotation(h, *rotationAngles) for h in snapshotOne[j]['vel']]) + relativeVel)
+                positions.append(np.array([rotation(h, *rotationAngles) for h in snapshotOne[i]['Coordinates']]) + relativePos)
+                velocities.append(np.array([rotation(h, *rotationAngles) for h in snapshotOne[i]['Velocities']]) + relativeVel)
             else:
-                positions.append(np.array(snapshotOne[j]['pos']) + relativePos)
-                velocities.append(np.array(snapshotOne[j]['vel']) + relativeVel)                
-            masses.append(np.array(snapshotOne[j]['mass']))
-            if i == 'gas':
-                energy.append(np.array(snapshotOne[j]['u']))
-                rho.append(np.array(snapshotOne[j]['rho']))
-                smoothing.append(np.array(snapshotOne[j]['smooth']))
+                positions.append(np.array(snapshotOne[i]['Coordinates']) + relativePos)
+                velocities.append(np.array(snapshotOne[i]['Velocities']) + relativeVel)                
+            masses.append(np.array(snapshotOne[i]['Masses']))
+            if i == 'PartType0':
+                energy.append(np.array(snapshotOne[i]['InternalEnergy']))
+                rho.append(np.array(snapshotOne[i]['Density']))
+                smoothing.append(np.array(snapshotOne[i]['SmoothingLength']))
+            if i != 'PartType1':
+                metallicity.append(np.concatenate(np.array(snapshotZero[i]['Metallicity'])))
         
         else:
             nPart.append(0)
@@ -164,7 +171,7 @@ def join (snapshotZero, snapshotOne, output='init.dat',
 
     dataList = []
 
-    if ('gas' in [*snapshotZero.families()]) or ('gas' in [*snapshotZero.families()]):
+    if ('PartType0' in [*snapshotZero.keys()]) or ('PartType0' in [*snapshotZero.keys()]):
         dataList = [np.concatenate(positions),
                     np.concatenate(velocities),
                     np.array(range(sum(nPart))) + 1,
@@ -183,11 +190,10 @@ def join (snapshotZero, snapshotOne, output='init.dat',
 
 
     #Shifting to center of mass
-    # TODO make this part more readable
     if shiftToCOM:
-        xCOM = sum(dataList[0][:, 0] * dataList[3]) / sum(dataList[0][:, 0])
-        yCOM = sum(dataList[0][:, 1] * dataList[3]) / sum(dataList[0][:, 1])
-        zCOM = sum(dataList[0][:, 2] * dataList[3]) / sum(dataList[0][:, 2])
+        xCOM  = sum(dataList[0][:, 0] * dataList[3]) / sum(dataList[0][:, 0])
+        yCOM  = sum(dataList[0][:, 1] * dataList[3]) / sum(dataList[0][:, 1])
+        zCOM  = sum(dataList[0][:, 2] * dataList[3]) / sum(dataList[0][:, 2])
 
         vxCOM = sum(dataList[1][:, 0] * dataList[3]) / sum(dataList[1][:, 0])
         vyCOM = sum(dataList[1][:, 1] * dataList[3]) / sum(dataList[1][:, 1])
@@ -198,7 +204,8 @@ def join (snapshotZero, snapshotOne, output='init.dat',
 
 
     #Changing the shape for writting
-    #the gadget format is composed of binary blocks of data which are written in a format like xyzxyz..xyz for positions and velocities
+    #the gadget format is composed of binary blocks of data which are written like xyzxyz..xyz for positions and velocities
+    # snapwrite is able to write this format of lists into either hdf5 or gadget2 format
     dataList[0].shape = (-1, 1)
     dataList[1].shape = (-1, 1)
 
@@ -212,33 +219,33 @@ def join (snapshotZero, snapshotOne, output='init.dat',
 
 if __name__ == '__main__':
 
-    import argparse
-
     parser = argparse.ArgumentParser(description='placeholder')
 
     parser.add_argument('snapshot0', help='The name of the first input file.')
     parser.add_argument('snapshot1', help='The name of the second input file.')
     parser.add_argument('-o', '--output', default='init.ic', help='The name of\
                         the output file')
-    parser.add_argument('-rP', '--relative-position', nargs='+',
-                        default=[0.0, 0.0, 0.0], help='Relative position of\
-                         the second snapshot with relation to the first.\
-                         Must be given in terms of it\'s components in x, y \
-                         and z')
-    parser.add_argument('-rV', '--relative-velocity', nargs='+',
-                        default=[0.0, 0.0, 0.0], help='Relative velocity of the\
-                         second snapshot with relation to the first. Must be \
-                         given in terms of it\'s components in x, y and z')
-    parser.add_argument('-r', '--rotation', nargs='+',
+    parser.add_argument('-rP', '--relative-position', nargs=3,
+                        metavar=('X', 'Y', 'Z'), default=[0.0, 0.0, 0.0],
+                        help='Relative position of the second snapshot with\
+                         relation to the first. Must be given in terms of it\'s\
+                         components in x, y and z')
+    parser.add_argument('-rV', '--relative-velocity', nargs=3,
+                        metavar=('vX', 'vY', 'vZ'), default=[0.0, 0.0, 0.0],
+                        help='Relative velocity of the second snapshot with\
+                         relation to the first. Must be given in terms of it\'s\
+                         components in x, y and z')
+    parser.add_argument('-r', '--rotation', nargs=3,
+                        metavar=('angleX', 'angleY', 'angleZ'),
                         default=[0.0, 0.0, 0.0], help='Angle of the rotation\
                          to be applied to the second snapshot. Must be given\
-                         in terms of rotations around the x, y and z axis that\
-                         pass by the origin of the second snapshot')
+                         in terms of rotations around the x\', y\' and z\'\
+                         axis that pass by the origin of the second snapshot')
     parser.add_argument('--hdf5', action='store_true', help='Output initial\
                         conditions in HDF5')
     parser.add_argument('--noMainHalo', action='store_false', help='This will\
                          make the program skip the halo of dark matter in the\
-                         first snapshot parsed.')
+                         first snapshot given.')
 
     args = parser.parse_args()
 
